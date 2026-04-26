770f69a20352bd1c76a7428515a3315082d317c3
Dome C. <dome@tel.co.th>
2026-04-26 23:45:21 +0700

    feat(tokenlist): add PaysoNow (3773) chain with PUSD and SAOMSO124 tokens

--- apps/tokenlist/src/chains.ts
+++ apps/tokenlist/src/chains.ts
@@ -5,6 +5,22 @@ export const tempoMainnet = tempo.extend({
 	feeToken: '0x20c0000000000000000000000000000000000000',
 })
 
+export const paysonow = defineChain({
+	id: 3773,
+	name: 'PaysoNow',
+	nativeCurrency: {
+		name: 'PUSD',
+		symbol: 'PUSD',
+		decimals: 6,
+	},
+	rpcUrls: {
+		default: {
+			http: ['https://rpc.paysonow.com'],
+		},
+	},
+	feeToken: '0x20c0000000000000000000000000000000000000',
+})
+
 export const yourChain = defineChain({
 	id: 7447,
 	name: 'Your Chain',
@@ -24,6 +40,7 @@ export const CHAIN_IDS = [
 	tempoDevnet.id,
 	tempoTestnet.id,
 	tempoMainnet.id,
+	paysonow.id,
 	yourChain.id,
 ] as const
 
@@ -31,5 +48,6 @@ export const chains = {
 	[tempoDevnet.id]: tempoDevnet,
 	[tempoTestnet.id]: tempoTestnet,
 	[tempoMainnet.id]: tempoMainnet,
+	[paysonow.id]: paysonow,
 	[yourChain.id]: yourChain,
 }
